f8dbf0a0bd14ef8760066e9b3cfe53e3e9c1ebf3
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-08-26 12:38:16 +0600

    student and teacher filters

--- academics/views.py
+++ academics/views.py
@@ -1,6 +1,11 @@
 from rest_framework import viewsets
 from .models import Student, Teacher, Class, Subject
 from .serializers import StudentSerializer, TeacherSerializer, ClassSerializer, SubjectSerializer
+from rest_framework import generics
+from django_filters import rest_framework as filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import TeacherFilter,StudentFilter
+
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
@@ -20,18 +25,6 @@ class SubjectViewSet(viewsets.ModelViewSet):
     serializer_class = SubjectSerializer
 
 
-from rest_framework import generics
-from django_filters import rest_framework as filters
-from django_filters.rest_framework import DjangoFilterBackend
-
-class StudentFilter(filters.FilterSet):
-    username = filters.CharFilter(field_name='username', lookup_expr='icontains')
-    roll_no = filters.NumberFilter(field_name='roll_no')
-
-    class Meta:
-        model = Student
-        fields = ['username', 'roll_no']
-        
         
 class StudentListView(generics.ListAPIView):
     queryset = Student.objects.all()
@@ -39,13 +32,6 @@ class StudentListView(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_class = StudentFilter
     
-    
-    
-from rest_framework import generics
-from django_filters.rest_framework import DjangoFilterBackend
-from .models import Teacher
-from .serializers import TeacherSerializer
-from .filters import TeacherFilter
 
 class TeacherListView(generics.ListAPIView):
     queryset = Teacher.objects.all()
@@ -54,20 +40,3 @@ class TeacherListView(generics.ListAPIView):
     filterset_class = TeacherFilter
     
     
-    
-    
-    
-from rest_framework import generics
-from rest_framework.response import Response
-from django_filters import rest_framework as filters
-from .models import Teacher
-from .serializers import TeacherSerializer
-
-class TeacherFilter(filters.FilterSet):
-    email = filters.CharFilter(field_name='email', lookup_expr='icontains')
-    subject = filters.NumberFilter(field_name='subject')
-    employee_id = filters.NumberFilter(field_name='employee_id')
-
-    class Meta:
-        model = Teacher
-        fields = ['employee_id', 'email', 'subject']
